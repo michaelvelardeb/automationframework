130e5f75e30fde91b826393571f64ee1d1353bc0
michaelvelardeb <michaelvelarde@yahoo.com>
2021-12-19 16:52:20 -0800

    add Logger

--- src/test/java/Utils/TestManager.java
+++ src/test/java/Utils/TestManager.java
@@ -8,16 +8,19 @@ import org.testng.Reporter;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.logging.Logger;
 
 
 public class TestManager {
     public DriverOperations dos;
     public Reporter reporter;
     public PropertyValues propertyValues;
+    public Logger logger;
 
     public static TestManager initializeTestManager() throws MalformedURLException {
         TestManager tm = new TestManager();
         tm.reporter = new Reporter();
+        tm.logger = Logger.getLogger("automation_logger");
         WebDriver driver = null;
 
         PropertyValues properties = new PropertyValues();
@@ -29,7 +32,8 @@ public class TestManager {
 
         System.setProperty("webdriver.chrome.driver","chromedriver.exe");
         switch (properties.browser) {
-            case "chrome" -> driver = new ChromeDriver(new ChromeOptions().addArguments("headless"));
+//            case "chrome" -> driver = new ChromeDriver(new ChromeOptions().addArguments("headless"));
+            case "chrome" -> driver = new ChromeDriver(new ChromeOptions());
 //            case "chrome" -> driver = new RemoteWebDriver(new URL("http://172.17.0.2:4444/wd/hub"),new ChromeOptions());
 //            case "firefox" -> driver = new FirefoxDriver();
         }
--- src/test/java/tests/TestMain.java
+++ src/test/java/tests/TestMain.java
@@ -4,8 +4,7 @@ import Utils.TestManager;
 import org.testng.annotations.*;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
-
-
+import java.util.logging.Level;
 
 public class TestMain {
     protected TestManager tm;
@@ -15,21 +14,22 @@ void setUpTestManager(Method method) {
    tm = new TestManager();
     try {
         tm = TestManager.initializeTestManager();
-//        tm.logger.log(Level.INFO, "**********************************************************"
-//                + "\nset up complete using browser: "
-//                + "\n" + tm.logger.getName()
-//                + "\nstarting test: " + method.getName()
-//                + "\n*******************************************************************************");
+        tm.logger.log(Level.INFO, "********************************************************************************************"
+                + "\nset up complete using browser: " + tm.propertyValues.getBrowser()
+                + "\n" + tm.logger.getName()
+                + "\nstarting test: " + method.getName()
+                + "\n***************************************************************************************************");
 
     } catch (MalformedURLException e) {
-//        tm.logger.log(Level.SEVERE, e.getMessage());
+        tm.logger.log(Level.SEVERE, e.getMessage());
     }
-
 }
 
 @AfterMethod(alwaysRun = true)
 void closeTestManager() {
-//        tm.logger.log(Level.INFO, "Test Complete Shutting down driver...............................");
+        tm.logger.log(Level.INFO, "Test Complete Shutting down driver...............................");
         tm.dos.closeDriver();
     }
+
+
 }
