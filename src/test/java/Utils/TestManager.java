16b48e787fe88a3c0da1bc7ab82a2982e440fba9
michaelvelardeb <megaGithub11!>
2022-01-15 15:38:34 -0800

    change DriverOperations to AutomationOperations to add Test Report support Add Extent Reports

--- src/test/java/Utils/TestManager.java
+++ src/test/java/Utils/TestManager.java
@@ -1,9 +1,12 @@
 package Utils;
 
+import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentReporter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Reporter;
 import java.io.IOException;
@@ -13,15 +16,13 @@ import java.util.logging.Logger;
 
 
 public class TestManager {
-    public DriverOperations dos;
+    public AutomationOperations ao;
     public Reporter reporter;
     public PropertyValues propertyValues;
-    public Logger logger;
 
-    public static TestManager initializeTestManager() throws MalformedURLException {
+    public static TestManager initializeTestManager(ExtentTest test) throws MalformedURLException {
         TestManager tm = new TestManager();
         tm.reporter = new Reporter();
-        tm.logger = Logger.getLogger("automation_logger");
         WebDriver driver = null;
 
         PropertyValues properties = new PropertyValues();
@@ -36,10 +37,12 @@ public class TestManager {
 //            case "chrome" -> driver = new ChromeDriver(new ChromeOptions().addArguments("headless"));
             case "chrome" -> driver = new ChromeDriver(new ChromeOptions());
             case "chromeR" -> driver = new RemoteWebDriver(new URL(properties.SELENIUM_HUB),new ChromeOptions());
-//            case "firefox" -> driver = new FirefoxDriver();
+            case "firefox" -> driver = new FirefoxDriver();
+            case "edge" -> driver = new EdgeDriver();
         }
 
-        tm.dos = DriverOperations.initializeDriverOperations(driver);
+//        tm.ao.logger = Logger.getLogger("automation_logger");
+        tm.ao = AutomationOperations.initializeAutomationOperations(driver, Logger.getLogger("automation_logger"), test);
         tm.propertyValues = properties;
         return tm;
 
--- src/test/java/tests/ExampleTest.java
+++ src/test/java/tests/ExampleTest.java
@@ -1,5 +1,7 @@
 package tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import java.util.Random;
@@ -8,7 +10,7 @@ public class ExampleTest extends TestMain {
 
     @Test(groups={"smoke", "smoke2", "smoke4"})
     public void exampleTest() {
-        tm.dos.navigateToUrl("http://www.google.com");
+        tm.ao.navigateToUrl("http://www.google.com");
         Random rd = new Random(); // creating Random object
         boolean status = rd.nextBoolean();
         Assert.assertTrue(status);
@@ -16,9 +18,12 @@ public class ExampleTest extends TestMain {
 
     @Test(groups="smoke3")
     public void exampleTest2() {
-        tm.dos.navigateToUrl("http://www.google.com");
+        tm.ao.navigateToUrl("http://www.google.com");
         Random rd = new Random(); // creating Random object
         boolean status = rd.nextBoolean();
+        tm.ao.enterText(By.name("q"), "espn boxing schedule");
+        tm.ao.enterText(By.name("q"), Keys.ENTER.toString());
+
         Assert.assertTrue(status);
     }
 }
